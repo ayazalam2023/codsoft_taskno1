20eb342d86ce376cf1aad7735baa22ea1ef73676
ayazalam2023 <ayumallick7079@gmail.com>
2024-09-11 11:54:23 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+// JavaScript (Save this in a file called script.js)
+document.addEventListener('DOMContentLoaded', function() {
+    const connectButton = document.querySelector('.connect-button');
+    connectButton.addEventListener('click', function() {
+        alert('Thank you for connecting with me!');
+    });
+});
